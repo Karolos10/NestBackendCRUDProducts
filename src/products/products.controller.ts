7f650facdc21b0c0324c5675afd943b7e3373015
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-09-11 12:17:18 -0500

    implementation of cors, swagger for visualisation and testing.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,32 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create product' })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Return all products.' })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ status: 200, description: 'Return a product by id.' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiResponse({ status: 200, description: 'Update a product by id.' })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiResponse({ status: 200, description: 'Delete a product by id.' })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
